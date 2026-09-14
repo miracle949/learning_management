d1b7885127313a306e6a5d9f23a7149fba710843
miracle949 <rogelioamoyan123@gmail.com>
2026-09-14 05:48:39 -0600

    commit

--- app/models/Students.php
+++ app/models/Students.php
@@ -463,7 +463,6 @@ class Students extends Model
         }
         return $grouped;
     }
-<<<<<<< HEAD
 
     // ── ARRANGE THE STEPS ──────────────────────────────────────
 
@@ -598,9 +597,7 @@ class Students extends Model
 
         return $grouped;
     }
-=======
     
->>>>>>> d2167049bf10889dd635a2e7f38f737a21869ea5
 
     public function getDragDropSubmission($lessonId, $gameTitle, $studentId)
     {
